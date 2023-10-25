f8abc670033325ee715e18bb3b6f5aa16feb8845
Akul16 <akuldpatel16@gmail.com>
2023-10-25 16:00:05 -0400

    i commit

--- server/index.js
+++ server/index.js
@@ -27,6 +27,12 @@ app.get('/', (req, res) => {
     res.send('hello to MEMORIES')
 })
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 import path from 'path'
 //const path = require("path");
 __dirname = path.resolve();
